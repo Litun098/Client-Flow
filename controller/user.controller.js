3eb88a3ada281e28c712bbec7f8ede1b6afa3cb4
Litun098 <nayaklitun9@gmail.com>
2022-12-27 19:48:03 +0530

    Added user controller

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,3 +1,4 @@
+const e = require('express');
 const User = require('../models/user.model');
 const ObjectConverter = require("../utils/constants")
 
@@ -60,4 +61,96 @@ const fetchByType = async (userTypeReq,res)=>{
         })
     }
     return users;
+}
+
+const fetchByStatus = async (userStatusReq,req)=>{
+    let users;
+
+    try{
+        users =await User.find({
+            userStatus:userStatusReq
+        })
+    }catch(err){
+        consolw.log(err);
+        res.status(500).send({
+            message:"Some internal error occured."
+        })
+    }
+    return users;
+}
+
+exports.findById = async (req,res)=>{
+    const userIdReq = req.params.userId;
+    let user;
+
+    try{
+        user = await User.find({
+            userId:userIdReq
+        })
+    }catch(err){
+        res.status(500).send({
+            message:"Internal server error"
+        })
+    }
+    if(user.length > 0){
+        res.status(200).send(ObjectConverter.userResponse(user));
+    }else{
+        res.status(200).send({
+            message:`User with this id [${userIdReq}] is not present`
+        })
+    }
+}
+
+
+/**
+ * Fetch the list of all users
+ */
+
+exports.findAll = async (req,res)=>{
+    let user;
+
+    let userTypeReq = req.body.userTypes;
+    let userStatusReq = req.body.userStatus;
+    let userNameReq = req.query.name;
+
+    if(userNameReq){
+        user = await fetchByName(userNameReq,res);
+    }else if(userTypeReq && userStatusReq){
+        user = await fetchByTypeAndStatus(userStatusReq,userStatusReq,res);
+    }else if(userTypeReq){
+        user = await fetchByType(userTypeReq,res);
+    }else if(userStatusReq){
+        user = await fetchByStatus(userStatusReq,res);
+    }else{
+        user = await fetchAll(res)
+    }
+    res.status(200).send(
+        ObjectConverter.userResponse(user)
+    )
+}
+
+exports.update = async (req,res)=>{
+    const userIdReq = req.params.userId;
+    try{
+        const user = await User.findOneAndUpdate({
+            userId:userIdReq
+        },{
+            userStatus:req.body.userStatus
+        }).exec()
+
+        if(user){
+            res.status(200).send({
+                message:`User record has been updated successfully.`
+            })
+        }else{
+            res.status(200).send({
+                message:"No user found with this Id"
+            })
+        }
+    }catch(err){
+        console.log("Error while updating the record",err);
+        res.status(500).send({
+            message:"Some internal error occured."
+        })
+    }
 }
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -3,6 +3,6 @@ const userController = require('../controller/user.controller');
 
 module.exports = function(app) {
     app.get('/crm/api/users/',[authJwt.verifyToken,authJwt.isAdmin],(req,res)=>{
-        userController.findAll)
+        userController.findAll()
     })
 }
